435f5922203600089d6eee142ce8c7129820d079
Antoine Aflalo <antoineaf@admincmd.com>
2014-07-12 19:26:34 +0300

    Frist version of the node

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Nodes
 The DataIn node is used to store information into the chosen collection. You select which collection you want to use and the path to the object. The path can be overridden with the msg.datapath. The msg.payload will contain the data you want to store.
 
 ###DataOut
-The DataOut node is used to retrieve the information from the chosen collection. As for the DataIn node you configure wich collection you want to use and the path where the data you need are stored. The path can also be overridden with msg.datapath. The msg.payload will be the object representing the stored datas.
+The DataOut node is used to retrieve the information from the chosen collection. As for the DataIn node you configure which collection you want to use and the path where the data you need are stored. The path can also be overridden with msg.datapath. The msg.payload will be the object representing the stored datas.
---
+++ json-db.js
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2014 Antoine Aflalo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function (RED) {
+    "use strict";
+    var fs = require("fs");
+    var JsonDB = require("node-json-db");
+
+    function JsonDBCollection(n) {
+        RED.nodes.createNode(this, n);
+        this.db = new JsonDB("JsonDB_" + n.collection, n.save);
+
+        this.on("close", function () {
+            this.db.save(true);
+        });
+    }
+
+    RED.nodes.registerType("json-db-collection", JsonDBCollection);
+
+    function DataIn(n) {
+        RED.nodes.createNode(this, n);
+        /**
+         * @var JsonDB
+         */
+        this.collection = RED.nodes.getNode(n.collection);
+        this.dataPath = n.path;
+        this.override = !n.update;
+        var node = this;
+
+        this.on("input", function (msg) {
+            var path = msg.datapath || node.dataPath;
+            try {
+                node.collection.db.push(path, msg.payload, node.override);
+            } catch (error) {
+                node.error(error);
+            }
+        });
+
+    }
+
+    RED.nodes.registerType("DataIn", DataIn);
+
+
+    function DataOut(n) {
+        RED.nodes.createNode(this, n);
+        /**
+         * @var JsonDB
+         */
+        this.collection = RED.nodes.getNode(n.collection);
+        this.dataPath = n.path;
+        var node = this;
+
+        this.on("input", function (msg) {
+            var path = msg.datapath || node.dataPath;
+            try {
+                var data = node.collection.db.getData(path);
+                msg.payload = data;
+                node.send(msg);
+            } catch (error) {
+                node.error(error);
+            }
+        });
+
+    }
+
+    RED.nodes.registerType("DataOut", DataOut);
+
+}
+
