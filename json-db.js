02603260d0ec6143d8b6555e9d4488d3923bd7c9
Antoine Aflalo <antoine@aaflalo.me>
2016-04-12 19:33:42 -0400

    Fix moving old files

--- json-db.js
+++ json-db.js
@@ -27,7 +27,12 @@ module.exports = function (RED) {
         try {
             var oldFile = path.join(process.cwd(), "JsonDB_" + n.collection + ".json");
             var stats = fs.statSync(oldFile);
-            fs.mkdirSync(defaultPath);
+            try {
+                stats = fs.statSync(defaultPath);
+            }catch(error) {
+                fs.mkdirSync(defaultPath);
+            }
+            
             fs.renameSync(oldFile, collectionFilePath);
             this.log("Moved old file from '" + oldFile + "' to '" + collectionFilePath + '"');
         } catch (error) {
